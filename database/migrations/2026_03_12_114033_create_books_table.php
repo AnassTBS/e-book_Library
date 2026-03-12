67f656cd96d741ba6f4a127239d8d2828e16f491
Ibrahim Benaaddi <benaaddiibrahim@gmail.com>
2026-03-12 12:14:42 +0000

    Update : i create books table by migration

---
+++ database/migrations/2026_03_12_114033_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string('book_name',100);
+            $table->mediumText('description')->nullable();
+            $table->string('author',100);
+            $table->year('release_date');
+            $table->string('image',255)->nullable();
+            $table->enum('category',["fiction", "action", "fantasy", "comedy", "non-fiction"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
